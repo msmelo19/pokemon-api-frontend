78dcb3daf1bcf3eeb889d7f24f82559daedcd023
Mateus Melo <msmelo95@gmail.com>
2021-04-20 19:36:12 -0300

    Adicionando o arquivo de cores da página

---
+++ water/src/config/colors.ts
@@ -0,0 +1,11 @@
+// Primary colors
+export const primaryColor = '#3ca2c3';
+export const primaryLightColor = '#75d3f6';
+export const primaryDarkColor = '#007392';
+export const primaryTextColor = '#000000';
+
+// Secondary colors
+export const secondaryColor = '#274b56';
+export const secondaryLightColor = '#537783';
+export const secondaryDarkColor = '#00232d';
+export const secondaryTextColor = '#ffffff';
